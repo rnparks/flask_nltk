32f78e76c9edd069fd6e326679e6781d744af60b
rnparks <rnparks@gmail.com>
2017-03-22 18:03:38 -0400

    database working

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Feynman1988@localhost/wordcount'
 
 class ProductionConfig(Config):
     DEBUG = False
